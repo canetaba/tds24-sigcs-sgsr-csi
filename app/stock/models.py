785db32fe24ace540ca0635b1091f2a0c55f2310
Obriel <obriel@MacBook-Pro-de-Obriel.local>
2024-08-02 01:51:33 -0400

    feat(stock/models.py): se crean las migraciones de la carpeta 'stock' para cumplir con la prueba 1b

--- app/stock/models.py
+++ app/stock/models.py
@@ -1,23 +1,43 @@
 from django.db import models
+from maestro.models import Medicamento, Institucion
+from datetime import date
 
 
 class Lote(models.Model):
-    medicamento = models.CharField(max_length=255)
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     fecha_vencimiento = models.DateField()
     codigo = models.CharField(max_length=255)
     cantidad = models.PositiveIntegerField()
+
     def __str__(self) -> str:
         return self.codigo
+
     pass
 
 
 class Consumo(models.Model):
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    fecha = models.DateField(default=date.today)
+    cantidad = models.PositiveIntegerField()
     pass
 
 
 class Stock(models.Model):
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=0)
+    fecha_actualizacion = models.DateField(default=date.today)
+    has_quiebre = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = [("institucion", "medicamento")]
+
     pass
 
 
 class Movimiento(models.Model):
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    fecha = models.DateField(default=date.today)
+    lote = models.OneToOneField(Lote, unique=True, on_delete=models.CASCADE)
     pass
