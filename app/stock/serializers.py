93dd8366bc077e5fb0cfc59ce5cdb540f0d773dd
Obriel <obriel@MacBook-Pro-de-Obriel.local>
2024-08-04 16:57:38 -0400

    stock/serializers: se crean serializadores para stock, consumo, lote y movimiento

--- app/stock/serializers.py
+++ app/stock/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 
 from .models import Stock, Lote, Consumo, Movimiento
 
+
 class LoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lote
         fields = ["id", "codigo", "medicamento", "cantidad", "fecha_vencimiento"]
 
+
 class ConsumoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Consumo
         fields = ["id", "institucion", "medicamento", "cantidad", "fecha"]
+
 
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,11 +21,11 @@ class StockSerializer(serializers.ModelSerializer):
         fields = ["id", "institucion", "medicamento", "cantidad", "has_quiebre", "fecha_actualizacion"]
 
     def get_unique_together_validators(self):
-        """ Se sobreescribe el metodo, para eliminar la validacion de 'unique_together' """
+        """Se sobreescribe el metodo, para eliminar la validacion de 'unique_together'"""
         return []
+
 
 class MovimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movimiento
         fields = ["id", "institucion", "lote", "fecha"]
-
