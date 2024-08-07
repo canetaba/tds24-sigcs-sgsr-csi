d50b79b1bedd7526b3c54f7338db71468aca4634
María José Briceño Nikulin <canetaba@users.noreply.github.com>
2024-08-07 00:03:58 -0400

    se agregan los modelos para el package stock

--- app/stock/models.py
+++ app/stock/models.py
@@ -1,17 +1,37 @@
 from django.db import models
+from maestro.models import Medicamento, Institucion
+from datetime import date
 
 
 class Lote(models.Model):
-    pass
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    fecha_vencimiento = models.DateField()
+    codigo = models.CharField(max_length=255)
+    cantidad = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.codigo
 
 
 class Consumo(models.Model):
-    pass
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    fecha = models.DateField(default=date.today)
+    cantidad = models.PositiveIntegerField()
 
 
 class Stock(models.Model):
-    pass
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=0)
+    fecha_actualizacion = models.DateField(default=date.today)
+    has_quiebre = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = [("institucion", "medicamento")]
 
 
 class Movimiento(models.Model):
-    pass
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
+    fecha = models.DateField(default=date.today)
+    lote = models.OneToOneField(Lote, unique=True, on_delete=models.CASCADE)
